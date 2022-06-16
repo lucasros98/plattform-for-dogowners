c19f8969ddb57486e2516dd01b2f6b5ec8175478
Lucas Rosvall <lucasrosvall@gmail.com>
2022-06-16 13:14:30 +0200

    added login modal and improved homepage

---
+++ frontend/components/LoginModal.js
@@ -0,0 +1,56 @@
+import React, {useEffect,useState} from "react";
+
+export default function LoginModal({visible,setVisible}) {
+    const [show,setShow] = useState(false)
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    useEffect(() => {
+        if(visible) setShow(true)
+    },[visible])
+
+    function close() {
+        setShow(false)
+        setVisible(false)
+    }
+
+    return (
+        <>{show ? <div id="top-right-modal" className="backdrop flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative p-4 w-full max-w-md  md:h-auto">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" onClick={() => close()}>
+                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </button>
+                    <div className="py-6 px-6 lg:px-8">
+                        <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Logga in på vår plattform</h3>
+                        <form className="space-y-6" action="#">
+                            <div>
+                                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Din email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="kalle@gmail.com" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+                            </div>
+                            <div>
+                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Ditt lösenord</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            </div>
+                            <div className="flex justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                                    </div>
+                                    <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Kom ihåg mig</label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Logga in</button>
+                            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                                Inte registerad? <a href="/create" className="text-blue-700 hover:underline dark:text-blue-500">Skapa konto</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        : null}
+        </>
+    )
+}
+
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,39 @@
 import Hero from '@/components/Hero'
+import FAQ from '@/components/FAQ'
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <div className=" bg-white ">
+        <div className='container  px-5 mx-auto'>
+          <section className="text-gray-700">
+            <div className="container px-5 py-16 mx-auto">
+              <div className="text-center mb-20">
+                <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+                  Vanliga frågor
+                </h1>
+                <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                  Nedan listar vi några av de vanligaste frågorna som vi brukar få.
+                </p>
+              </div>
+              <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                <div className="w-full lg:w-1/2 px-4 py-1">
+                <FAQ question={"Behöver man ha valp för att använda er tjänst?"} content={"Du behöver inte ha en valp för att använda vår tjänst. Alla hundälskare är välkomna."} />
+                  <FAQ question={"Varför finns dinhundvalp?"} content={"Att skaffa hundvalp kan vara svårt, det var något som vi ville hjälpa nya hundägare med."} />
+                  <FAQ question={"Hur skapar jag ett konto?"} content={"Du kan skapa konto genom att klicka på Skapa konto knappen."} />
+                </div>
+                <div className="w-full lg:w-1/2 px-4 py-1">
+                  <FAQ question={"Kan man få rådgivning från er?"} content={"Nej, vi ger ingen rådgivning till hundägare."} />
+                  <FAQ question={"Har ni digitala valpkurser?"} content={"I nuläget har vi inga digitala hundkurser för hundägare."} />
+                  <FAQ question={"Har ni en app för mobilen?"} content={"Just nu finns vi endast som webbtjänst."} />
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
 
-    
     </div>
   )
 }
