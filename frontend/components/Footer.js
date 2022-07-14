5454754b59f6eb7e599921a2fb312504eae8a972
Lucas Rosvall <lucasrosvall@gmail.com>
2022-07-14 10:07:33 +0200

    improved footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,22 +1,58 @@
+import React from "react";
 import Link from "next/link";
 
-export default function Footer() {
+export default function Footer(props) {
+  return (
+    <footer className="text-gray-600 body-font border-t">
+      <div className="container px-5 py-16 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+        <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 ">
+          <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <span className="text-xl">Hundvalpen</span>
+          </div>
+          <p className="mt-2 text-sm text-gray-500">En plattform för att hjälpa nya hundägare med sin valp.</p>
+        </div>
+        <div className="flex-grow flex flex-wrap md:pr-20 md:text-left text-center order-first">
+          <div className="lg:w-1/2 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Innehåll</h2>
+            <nav className="list-none mb-10">
+              <li>
+                <Link href={"/"} className="text-gray-600 hover:text-gray-800">Hem</Link>
+              </li>
+              <li>
+                <Link href={"/forum"} className="text-gray-600 hover:text-gray-800">Forum</Link>
+              </li>
+              <li>
+                <Link href={"/quiz"} className="text-gray-600 hover:text-gray-800">Quiz</Link>
+              </li>
+            </nav>
+          </div>
 
-    return (
-        <footer  className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-        <span  className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 Skaffa hundvalp. All Rights Reserved.
-        </span>
-        <ul  className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <Link href="/forum"  className="mr-4 hover:underline md:mr-6">Forum</Link>
-            </li>
-            <li>
-                <Link href="/quiz"  className="mr-4 hover:underline md:mr-6 ">Quiz</Link>
-            </li>
-            <li>
-                <Link href="/about"  className="mr-4 hover:underline md:mr-6">Om oss</Link>
-            </li>
-        </ul>
+          <div className="lg:w-1/2 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Andra länkar</h2>
+            <nav className="list-none mb-10">
+              <li>
+                <Link href={"/om"} className="text-gray-600 hover:text-gray-800">Om oss</Link>
+              </li>
+          
+              <li>
+                <Link href="/login" className="text-gray-600 hover:text-gray-800">Logga in</Link>
+              </li>
+              <li>
+                <Link href="/signup" className="text-gray-600 hover:text-gray-800">Skapa konto</Link>
+              </li>
+          
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-100">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-gray-500 text-sm text-center sm:text-left">© 2022 —
+            <Link href="/" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">Hundvalpen</Link>
+          </p>
+
+        </div>
+      </div>
     </footer>
-    )
+  );
 }
