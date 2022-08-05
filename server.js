50254a96b958832dc2e086ebf5ffb96f55b2bdb4
Lucas Rosvall <lucasrosvall@gmail.com>
2022-08-05 11:55:39 +0200

    added procfile

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 
 const session = require("express-session");
 const MongoStore = require("connect-mongo")
-const {Connection} = require('./backend/database/connection')
+const { Connection } = require('./backend/database/connection')
 
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
@@ -25,38 +25,38 @@ const hostname = process.env.HOST || "localhost"
 const PORT = process.env.PORT || 3000
 
 
-    const server = express();
+const server = express();
 
-    // Express Session
-    server.use(
-      session({
-          secret: process.env.JWT_TOP_SECRET,
-          resave: false,
-          saveUninitialized: true,
-          store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
-      })
-    );
+// Express Session
+server.use(
+  session({
+    secret: process.env.JWT_TOP_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
+  })
+);
 
-    // Passport middleware
-    server.use(passport.initialize());
-    server.use(passport.session());
+// Passport middleware
+server.use(passport.initialize());
+server.use(passport.session());
 
-    // support parsing of application/json type post data
-    server.use(bodyParser.json());
-    server.use(cookieParser());
+// support parsing of application/json type post data
+server.use(bodyParser.json());
+server.use(cookieParser());
 
 
-    //support parsing of application/x-www-form-urlencoded post data
-    server.use(bodyParser.urlencoded({ extended: true }));
+//support parsing of application/x-www-form-urlencoded post data
+server.use(bodyParser.urlencoded({ extended: true }));
 
-    server.use(router);
-    router.use(morgan('combined'))
+server.use(router);
+router.use(morgan('combined'))
 
-  
-    server.listen(PORT, () => {
-      console.log(`listening on PORT ${PORT}`);
-    });
-  
-  
-  
+
+server.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
+
+
+
 module.exports = server
